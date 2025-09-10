36b620bf125093ede46d2cdb2581cf6069470171
loopermallee <whisperingwoods91@gmail.com>
2025-09-10 21:23:45 +0800

    Update text_service.dart

--- lib/services/text_service.dart
+++ lib/services/text_service.dart
@@ -13,14 +13,14 @@ class TextService {
   static List<String> list = [];
   static List<String> sendReplys = [];
 
-  TextService._(); 
+  TextService._();
 
   Future startSendText(String text) async {
     isRunning = true;
 
     _currentLine = 0;
     list = EvenAIDataMethod.measureStringList(text);
-   
+
     if (list.length < 4) {
       String startScreenWords =
           list.sublist(0, min(3, list.length)).map((str) => '$str\n').join();
@@ -53,23 +53,22 @@ class TextService {
       _currentLine = 0;
       await updateReplyToOSByTimer();
     } else {
-      clear(); 
+      clear();
     }
   }
 
   int retryCount = 0;
   Future<bool> doSendText(String text, int type, int status, int pos) async {
-   
-    print('${DateTime.now()} doSendText--currentPage---${getCurrentPage()}-----text----$text-----type---$type---status---$status----pos---$pos-');
-    if (!isRunning) {
-      return false;
-    }
+    if (!isRunning) return false;
 
-    bool isSuccess = await Proto.sendEvenAIData(text,
-        newScreen: EvenAIDataMethod.transferToNewScreen(type, status),
-        pos: pos,
-        current_page_num: getCurrentPage(),
-        max_page_num: getTotalPages()); // todo pos
+    bool isSuccess = await Proto.sendEvenAIData(
+      text,
+      newScreen: EvenAIDataMethod.transferToNewScreen(type, status),
+      pos: pos,
+      current_page_num: getCurrentPage(),
+      max_page_num: getTotalPages(),
+    );
+
     if (!isSuccess) {
       if (retryCount < maxRetry) {
         retryCount++;
@@ -86,25 +85,19 @@ class TextService {
   Future updateReplyToOSByTimer() async {
     if (!isRunning) return;
     int interval = 8; // The paging interval can be customized
-   
+
     _timer?.cancel();
     _timer = Timer.periodic(Duration(seconds: interval), (timer) async {
-
       _currentLine = min(_currentLine + 5, list.length - 1);
       sendReplys = list.sublist(_currentLine);
 
       if (_currentLine > list.length - 1) {
         _timer?.cancel();
         _timer = null;
-
         clear();
       } else {
         if (sendReplys.length < 4) {
-          var mergedStr = sendReplys
-              .sublist(0, sendReplys.length)
-              .map((str) => '$str\n')
-              .join();
-
+          var mergedStr = sendReplys.map((str) => '$str\n').join();
           if (_currentLine >= list.length - 5) {
             await doSendText(mergedStr, 0x01, 0x70, 0);
             _timer?.cancel();
@@ -131,38 +124,27 @@ class TextService {
   }
 
   int getTotalPages() {
-    if (list.isEmpty) {
-      return 0;
-    }
-    if (list.length < 6) {
-      return 1;
-    }
+    if (list.isEmpty) return 0;
+    if (list.length < 6) return 1;
     int pages = 0;
     int div = list.length ~/ 5;
     int rest = list.length % 5;
     pages = div;
-    if (rest != 0) {
-      pages++;
-    }
+    if (rest != 0) pages++;
     return pages;
   }
 
   int getCurrentPage() {
-    if (_currentLine == 0) {
-      return 1;
-    }
+    if (_currentLine == 0) return 1;
     int currentPage = 1;
     int div = _currentLine ~/ 5;
     int rest = _currentLine % 5;
     currentPage = 1 + div;
-    if (rest != 0) {
-      currentPage++;
-    }
+    if (rest != 0) currentPage++;
     return currentPage;
   }
 
   Future stopTextSendingByOS() async {
-    print("stopTextSendingByOS---------------");
     isRunning = false;
     clear();
   }
