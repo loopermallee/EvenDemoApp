63da25237142eeaaac30fab39eec34ef4e374958
loopermallee <whisperingwoods91@gmail.com>
2025-09-12 00:23:51 +0800

    Delete android/app/src/main/kotlin/com/example/demo_ai_even/bluetooth/ChannelRefs.kt

--- android/app/src/main/kotlin/com/example/demo_ai_even/bluetooth/ChannelRefs.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demo_ai_even.bluetooth
-
-import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodChannel
-
-// DO NOT wrap these inside any class/object/companion.
-// They must be top-level so BleManager.kt can see them.
-lateinit var bleMC: MethodChannel
-lateinit var bleReceive: EventChannel
