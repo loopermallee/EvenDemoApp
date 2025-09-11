c40ac6358c1aad85099e29c8277ed1571d3bd7d6
loopermallee <whisperingwoods91@gmail.com>
2025-09-12 00:09:35 +0800

    Update ChannelRefs.kt

--- android/app/src/main/kotlin/com/example/demo_ai_even/bluetooth/ChannelRefs.kt
+++ android/app/src/main/kotlin/com/example/demo_ai_even/bluetooth/ChannelRefs.kt
@@ -3,6 +3,7 @@ package com.example.demo_ai_even.bluetooth
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 
-// Global channel refs so BleManager.kt can call into Flutter
+// DO NOT wrap these inside any class/object/companion.
+// They must be top-level so BleManager.kt can see them.
 lateinit var bleMC: MethodChannel
 lateinit var bleReceive: EventChannel
