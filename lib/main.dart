a309e7367e397e2f674a5f5c3ee57ecf18614045
loopermallee <whisperingwoods91@gmail.com>
2025-09-13 11:51:55 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,43 +2,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-// Your own files (use relative imports so package name changes don’t break)
-// If you don’t have BleTestPage yet, you can comment that import & route.
-import 'app.dart';
+import 'app.dart'; // MyApp + App singleton
 import 'controllers/evenai_model_controller.dart';
-import 'views/home_page.dart';
-import 'pages/ble_test_page.dart'; // <-- comment out if you didn’t create it
 
 void main() {
+  // Make sure Flutter is fully ready before we touch channels/controllers
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Register global state (EvenAI model/controller)
+  // Register your EvenAI state controller once, keep it alive for whole app
   Get.put(EvenaiModelController(), permanent: true);
 
-  // Make App singleton available early (even if it’s just helpers)
+  // Touch the App singleton (handy if you later add global boot logic)
   App.get;
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Even AI Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      getPages: [
-        GetPage(name: '/', page: () => const HomePage()),
-        // Remove this route if you didn’t add the test screen
-        GetPage(name: '/bleTest', page: () => const BleTestPage()),
-      ],
-    );
-  }
 }
