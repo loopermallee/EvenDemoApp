e134d3ad83a921cf7295b4b2a0e7c93d84b0b736
loopermallee <whisperingwoods91@gmail.com>
2025-09-14 17:38:43 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,45 @@
-
-import 'package:demo_ai_even/ble_manager.dart';
-import 'package:demo_ai_even/controllers/evenai_model_controller.dart';
-import 'package:demo_ai_even/views/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
+import 'home_page.dart'; // your starting screen
 
 void main() {
-  BleManager.get();
-  Get.put(EvenaiModelController());
-  
-  runApp(MyApp());
+  runApp(const EvenDemoApp());
 }
 
-class MyApp extends StatelessWidget {
+class EvenDemoApp extends StatelessWidget {
+  const EvenDemoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Even AI Demo',
+      title: 'Even Demo App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        primaryColor: Colors.greenAccent,
+        fontFamily: 'PressStart2P',
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.greenAccent),
+          bodyMedium: TextStyle(color: Colors.greenAccent),
+          bodySmall: TextStyle(color: Colors.greenAccent),
+          headlineMedium: TextStyle(color: Colors.greenAccent),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.greenAccent,
+            side: const BorderSide(color: Colors.greenAccent, width: 2),
+            textStyle: const TextStyle(fontFamily: 'PressStart2P', fontSize: 12),
+          ),
+        ),
+        snackBarTheme: const SnackBarThemeData(
+          backgroundColor: Colors.black,
+          contentTextStyle: TextStyle(
+            color: Colors.greenAccent,
+            fontFamily: 'PressStart2P',
+          ),
+        ),
       ),
-      home: HomePage(), 
+      home: const HomePage(),
     );
   }
 }
